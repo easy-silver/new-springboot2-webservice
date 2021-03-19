073fe82b321a948e00f6b8e7561eebf4fb776c95
Timo <developer.timo@gmail.com>
2021-03-19 17:29:38 +0900

    게시글 수정, 조회 기능 추가

--- src/main/java/com/timo/springboot/service/posts/PostsService.java
+++ src/main/java/com/timo/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.timo.springboot.service.posts;
 
+import com.timo.springboot.domain.posts.Posts;
 import com.timo.springboot.domain.posts.PostsRepository;
+import com.timo.springboot.web.dto.PostsResponseDto;
 import com.timo.springboot.web.dto.PostsSaveRequestDto;
+import com.timo.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,5 +18,21 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
